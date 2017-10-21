48aad4a7cf78657522df3f02cf425661237e082f
Your Name <you@example.com>
2017-10-21 14:46:28 +0200

    wersja3

---
+++ js/app.js
@@ -0,0 +1,47 @@
+// console.log("działa!");
+// console.log(4);
+// var myText1="lalala";
+// console.log(myText1);
+// //wartosci logiczne
+// var prawda = true;
+// var falsz = false;
+// console.log(prawda);
+// //tablice
+// var tablica = [1,2,3]; //ile jest elementow -3
+// //a na jakiej pozycji jest 2 -na 1 bo zaczynamy od zera
+// console.log(tablica[1]);
+// //pobrac dlugosc tablicy
+// console.log(tablica.length);
+// var tablica2 = ['jablko','gruszka'];
+// console.log(tablica2);
+// console.log(tablica2[1])
+// //funkcje
+// function wypisywanie(owoc){ //var owoc="marchewka"
+//   console.log("moja funkcja do wypisywania",  owoc);
+// return console.log()}
+// wypisywanie("marchewka"); //uruchamianie funkcji albo wywolywanie;
+// wypisywanie ("pomidor")
+// //console.log(owoc)
+// //return  var sok='miksowanie' +sok; return sok;
+// //return uzywamy zeby zwrocic co wrzucilismy w nawias
+// //miksowanie' + owoc=====sok======wypisywanie ('marchewka)======otrzymany sok')
+// //instrukcje warunkowe
+// var czyPada =true;
+// if( czyPada){console.log("wez parasol")}
+//  else{console.log("wez okulary pprzeciwsloneczne")}
+//
+//  //petle
+//  //++znaczy zwieksz o 1
+//  for(var i=0; i<10;i++ ){console.log("czesc", i );}
+//  //mozna tez wywolac funkcje np israin ona ma swojego konsologa
+
+
+var mojeimie = "Magda";
+console.log(mojeimie)
+var tablica = ['Magda','Natalia','Magda', 'Basia']
+console.log(tablica[1])
+ var liczba1 =5
+ var liczba2 =10
+ if (liczba1>liczba2){console.log("hurra!")
+
+ //DOM Api
